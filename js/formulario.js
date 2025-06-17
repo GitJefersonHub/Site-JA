c1f7a4d7a57f702c428286313cdffaabed748b36
“GitJefersonHub” <jefersonalves.ti@gmail.com>
2025-06-17 09:30:49 -0300

    09 30

--- js/formulario.js
+++ js/formulario.js
@@ -1,17 +1,1 @@
-document.getElementById("form-contato").addEventListener("submit", function(event) {
-    event.preventDefault();
-    const nome = document.getElementById("nome").value;
-    const email = document.getElementById("email").value;
-    const telefone = document.getElementById("telefone").value;
-    const mensagem = document.getElementById("mensagem").value;
-  
-    fetch("https://script.google.com/macros/s/AKfycbzO6pYYHLJd95uL8ZjX6YKq2rw6LS3wmcAERlwFSDJr/dev", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ nome, email, telefone, mensagem })
-    })
-    .then(response => response.text())
-    .then(data => alert(data))
-    .catch(error => console.error("Erro:", error));
-  });
-  
+
